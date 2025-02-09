84139d0b382c7a4fdb5005a0abfa9aaecad8994d
Kanchi Gaikwad <kanchan.gaikwad0230@gmail.com>
2025-02-09 21:56:38 +0530

    added input filled

--- studentdata/src/CreateStudent.js
+++ studentdata/src/CreateStudent.js
@@ -1,21 +1,26 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function CreateStudent() {
+    const[id , setId]=useState("");
+    const[name , setName]=useState("");
+    const[place , setPlace]=useState("");
+    const[phone , setPhone]=useState("");
     return (
      <div className="container">
         <h2>Add New Student</h2>
         <form>
             <label htmlFor="id">Id:</label>
-            <input type="text" id="id" name="id" />
+            <input type="text" id="id" name="id" value={id}  onChange={e => setId(e.target.value)}/>
 
             <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" />
+            <input type="text" id="name" name="name"  value={name} onChange={e => setName(e.target.value)}/>
             
             <label htmlFor="place">Place:</label>
-            <input type="text" id="place" name="place" />
+            <input type="text" id="place" name="place" value={place} onChange={e => setPlace(e.target.value)} />
 
             <label htmlFor="phone">Phone:</label>
-            <input type="text" id="phone" name="phone" />
+            <input type="text" id="phone" name="phone"  value={phone} onChange={e => setPhone(e.target.value)}/>
 
             <div>
             <button className="btn btn-save">Save</button>
