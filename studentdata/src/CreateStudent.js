20dd7088f2a53cdcfb58ebbaa8ba0a1d51cabf2a
Kanchi Gaikwad <kanchan.gaikwad0230@gmail.com>
2025-02-09 21:20:20 +0530

    added html form

--- studentdata/src/CreateStudent.js
+++ studentdata/src/CreateStudent.js
@@ -1,5 +1,28 @@
+import { Link } from "react-router-dom"
+
 export default function CreateStudent() {
     return (
-       <h1>Creating Student</h1>
+     <div className="container">
+        <h2>Add New Student</h2>
+        <form>
+            <label htmlFor="id">Id:</label>
+            <input type="text" id="id" name="id" />
+
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" />
+            
+            <label htmlFor="place">Place:</label>
+            <input type="text" id="place" name="place" />
+
+            <label htmlFor="phone">Phone:</label>
+            <input type="text" id="phone" name="phone" />
+
+            <div>
+            <button className="btn btn-save">Save</button>
+            <Link to= "/" className="btn btn-back">Back</Link>
+
+            </div>
+        </form>
+     </div>
     )
 }
