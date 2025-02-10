a301d0947c0d19af557c239ffd3190a25edcf40c
Kanchi Gaikwad <kanchan.gaikwad0230@gmail.com>
2025-02-10 17:45:31 +0530

    added able to get the data

--- studentdata/src/ViewDetails.js
+++ studentdata/src/ViewDetails.js
@@ -1,18 +1,24 @@
+import { useEffect , useState} from "react";
 import { Link, useParams } from "react-router-dom";
 
 export default function ViewDetails() {
-// console.log(useParams);
 const {studentid} = useParams();
-// console.log(studentid);
+const[studentData , setStudentData]=useState({});
+useEffect(()=>{
+    fetch("http://localhost:8000/students/"+studentid)
+    .then((res) => res.json())
+    .then((data) => setStudentData(data))
+    .catch((err) => console.log(err.message))
+},[]);
     return(
         <div className="container">
             <h1>Student Details</h1>
-            <div className="details">
-            <p><strong>Id:</strong>1</p>
-            <p><strong>Name:</strong>Pratik</p>
-            <p><strong>Place:</strong>Loni</p>
-            <p><strong>Phone:</strong>7575757575</p>
-        </div>
+            {studentData && <div className="details">
+            <p><strong>Id:</strong>{studentData.id}</p>
+            <p><strong>Name:</strong>{studentData.name}</p>
+            <p><strong>Place:</strong>{studentData.place}</p>
+            <p><strong>Phone:</strong>{studentData.phone}</p>
+        </div>}
         <Link to="/" class="btn btn-back">Back</Link>
         </div>
     )
